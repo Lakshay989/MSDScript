4bb26ed996421882d10ff50940ef5219390b3504
Lakshay989 <lakshaykucheriya@gmail.com>
2023-01-23 15:34:26 -0700

    Working on the expression classes

---
+++ MSDScript/MSDScript/expr.cpp
@@ -0,0 +1,55 @@
+//
+//  expr.cpp
+//  MSDScript
+//
+//  Created by Lakshay Santosh Kucheriya on 1/23/23.
+//
+
+#include "expr.hpp"
+
+
+Num::Num(int val)
+{
+    this->val = val;
+}
+
+bool Num::equals(Expr *e)
+{
+    Num* n = dynamic_cast<Num*>(e);
+    if (n == NULL)
+        return false;
+    else
+        return val == n->val;
+}
+
+Add::Add(Expr *lhs, Expr *rhs)
+{
+    this->lhs = lhs;
+    this->rhs = rhs;
+}
+    
+bool Add::equals(Expr *e)
+{
+    Add* a = dynamic_cast<Add*>(e);
+    if (a == NULL)
+        return false;
+    else
+        return (lhs->equals(a->lhs)
+                && rhs->equals(a->rhs));
+    }
+
+Mult::Mult(Expr *lhs, Expr *rhs)
+{
+    this->lhs = lhs;
+    this->rhs = rhs;
+}
+
+bool Mult::equals(Expr* e) {
+  Mult* m = dynamic_cast<Mult*>(e);
+  if (m == NULL)
+    return false;
+  else
+    return (lhs->equals(m->lhs)
+            && rhs->equals(m->rhs));
+}
+
---
+++ MSDScript/MSDScript/expr.hpp
@@ -0,0 +1,50 @@
+//
+//  expr.hpp
+//  MSDScript
+//
+//  Created by Lakshay Santosh Kucheriya on 1/23/23.
+//
+
+#ifndef expr_hpp
+#define expr_hpp
+
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+class Expr
+{
+public:
+    virtual bool equals(Expr *e) = 0;
+};
+
+class Num : public Expr
+{
+public:
+    int val;
+    
+    Num(int val);
+    bool equals(Expr *e) ;
+};
+
+class Add : public Expr
+{
+public:
+    Expr *lhs;
+    Expr *rhs;
+    
+    Add(Expr *lhs, Expr *rhs);
+    bool equals(Expr *e) ;
+};
+
+class Mult : public Expr
+{
+public:
+    Expr *lhs;
+    Expr *rhs;
+    
+    Mult(Expr *lhs, Expr *rhs);
+    bool equals(Expr *e) ;
+};
+
+#endif /* expr_hpp */
